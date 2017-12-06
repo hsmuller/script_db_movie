95689b4357b5c6fa59a431e6cb9793bb282cfebf
hsmuller <hsmuller@163.com>
2017-12-06 17:42:14 +0800

    get

--- fh/down_tor.py
+++ fh/down_tor.py
@@ -1,11 +1,14 @@
 # -*- coding:utf-8 -*-
 
+import requests
 from sys import argv
 from get_fh import *
 from get_cl import *
+from cl2zz import *
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
+
 
 def ny2cl(ny_name):
 	search_result = {}
@@ -16,11 +19,19 @@ def ny2cl(ny_name):
  			search_result[fh_name] = magnet
  	return search_result
 
+def download_zz(fh_name,magnet):
+	zz_url = cl2zz(magnet)
+	r = requests.get(zz_url)
+	if r.status_code == 200:
+		with open('zz/' + fh_name + '.torrent','wb') as code :
+			code.write(r.content)
+	else:
+		pass
 
 if __name__ == '__main__':
  	ny_name = argv[1]
  	search_result = ny2cl(ny_name)
  	for (fh_name,magnet) in search_result.items():
- 		print fh_name
- 		print magnet
-
+ 		download_zz(fh_name,magnet)
+ 		print "done"
+ 	raw_input()
